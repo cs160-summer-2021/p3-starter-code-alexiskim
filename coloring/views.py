13b0dfac8ccf971851a4fd77752c7e436bd0bc58
Alexis Kim <alexis-kim@berkeley.edu>
2021-07-13 19:50:14 -0700

    made two brushes

--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def new_interaction(request):
+    return render(request, 'coloring/new_interaction.html')
+
+def varied_width_brush(request):
+    return render(request, 'coloring/varied_width_brush.html')
+
+def flat_brush(request):
+    return render(request, 'coloring/flat_brush.html')
